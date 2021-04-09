58d8352f414f9239725f830b9bbc71378e95abe2
ne-plusultra <60425996+ne-plusultra@users.noreply.github.com>
2021-04-09 22:46:01 +0600

    master

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  text = 'Copyright @KazakhWiki by Nurik)';
+  text = 'Copyright @KazakhWiki by Nurik';
 
   constructor() { }
 
